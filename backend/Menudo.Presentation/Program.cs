bc58faf2535a503349125e9cd1a5ffec5b5d38c3
apiolli <cesardiaz.miduho@gmail.com>
2026-08-07 14:31:13 -0400

    backend: add Scalar reference and the ExceptionMiddleware to the pipeline

--- backend/Menudo.Presentation/Program.cs
+++ backend/Menudo.Presentation/Program.cs
@@ -1,19 +1,20 @@
+using Menudo.Presentation.Middlewares;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
